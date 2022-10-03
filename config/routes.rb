020f0028b6af33bda7c776fe42c3bf8f787cde85
psarge2 <paul@psarge.dev>
2022-10-03 15:16:26 -0400

    created two pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  # get 'home/index'
+  get 'home/team'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
